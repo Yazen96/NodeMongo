d04d24136f2475578234c84a00b37a67545302bb
Yazen96 <yazan.alnsierat@iths.se>
2025-01-20 11:49:17 +0100

    Added Mongo

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const port = 3000;
 
+app.get('/', (req, res) => {
+    res.send('Welcome to MongoAPI');
+    });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+    app.post("/blogpost", (req, res) => {
+        res.send("This is a post request");
+    });
+
+    app.put("/blogput", (req, res) => {
+        res.send("This is a put request");
+    });
+
+mongoose.
+connect('mongodb+srv://Yazen:Yazen123@pokemondatabase.bobmq.mongodb.net/Products-API?retryWrites=true&w=majority&appName=PokemonDataBase')
+.then(() => {
+    console.log('Connected to the PokemonDataBase!');
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
 });
